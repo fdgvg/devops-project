8c45ee569e6a213a3657cd98614ab7e8cdb91d05
fdgvg <sdfa@outlook.in>
2025-04-03 14:39:09 +0530

    Fix: renamed dev App Service to make name globally unique

--- infra/main.tf
+++ infra/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_app_service_plan" "plan" {
 }
 
 resource "azurerm_app_service" "dev" {
-  name                = "devops-app-dev"
+  name                = "devops-app-dev-karthick"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.plan.id
