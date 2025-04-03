b542c46837f5d578e31f41d65e53e4f049018789
fdgvg <sdfa@outlook.in>
2025-04-03 13:57:50 +0530

    added tf file

---
+++ infra/main.tf
@@ -0,0 +1,43 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-rg"
+  location = "Central India"
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "devops-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "dev" {
+  name                = "devops-app-dev"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
+  site_config {
+    linux_fx_version = "NODE|18-lts"
+  }
+}
+
+resource "azurerm_app_service" "qa" {
+  name                = "devops-app-qa"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
+  site_config {
+    linux_fx_version = "NODE|18-lts"
+  }
+}
